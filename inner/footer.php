a785267482731764188944adf11444df472b06c6
he11scream <qwerty.never.more@gmail.com>
2023-04-03 19:59:48 +0300

    layout

---
+++ inner/footer.php
@@ -0,0 +1,109 @@
+<!-- workarea -->
+					</div>
+				</div>
+				<div class="sb_sidebar">
+					<div class="sb_nav">
+						<ul>
+							<li class="close"><a href=""><span>Êàòàëîã</span></a></li>
+							<li class="close"><a href=""><span>Êóõíè</span></a></li>
+							<li class="close"><a href=""><span>Ãàðíèòóðû</span></a></li>
+							<li class="open current">
+								<span class="sb_showchild"></span>
+								<a href=""><span>Ñïàëüíè</span></a>
+								<ul>
+									<li><a href="">Îäíîìåñòðûå</a></li>
+									<li><a href="">Äâóõìåñòíûå</a></li>
+									<li><a href="">Äåòñêèå</a></li>
+								</ul>
+							</li>
+							<li class="close"><a href=""><span>Êóõíè</span></a></li>
+							<li class="close"><a href=""><span>Ãàðíèòóðû</span></a></li>
+							<li class="close">
+								<span class="sb_showchild"></span>
+								<a href=""><span>Ñïàëüíè</span></a>
+								<ul>
+									<li><a href="">Îäíîìåñòðûå</a></li>
+									<li><a href="">Äâóõìåñòíûå</a></li>
+									<li><a href="">Äåòñêèå</a></li>
+								</ul>
+							</li>
+						</ul>
+					</div>
+					<div class="sb_event">
+						<div class="sb_event_header"><h4>Áëèæàéøèå ñîáûòèÿ</h4></div>
+						<p><a href="">29 àâãóñòà 2012, Ìîñêâà</a></p>
+						<p>Ñåìèíàð ïðîèçâîäèòåëåé ìåáåëè Ðîññèè è ÑÍÃ, Îáñóæäåíèå òåíäåíöèé.</p>
+					</div>
+					<div class="sb_action">
+						<a href=""><img src="content/11.png" alt=""/></a>
+						<h4>Àêöèÿ</h4>
+						<h5><a href="">Ìåáåëüíàÿ ïîëêà âñåãî çà 560 Ð</a></h5>
+						<a href="" class="sb_action_more">Ïîäðîáíåå &rarr;</a>
+					</div>
+					<div class="sb_reviewed">
+							<img src="content/8.png" class="sb_rw_avatar" alt=""/>
+							<span class="sb_rw_name">Ñåðãåé Àíòîíîâ</span>
+							<span class="sb_rw_job">Ðóêîâîäèòåëü ôèíàíñîâîãî îòäåëà “Áàíê+”</span>
+							<p>“Ïîêóïàë îôèñíûå ñòóëüÿ è ñòîëû, îñòàëñÿ î÷åíü äîâîëåí! Íèçêèå öåíû, áûñòðàÿ äîñòàâêà, îáñëóæèâàíèå íà âûñîòå! Ñïàñèáî!”</p>
+							<div class="clearboth"></div>
+							<div class="sb_rw_arrow"></div>
+					</div>
+				</div>
+				<div class="clearboth"></div>
+			</div>
+		</div>
+
+		<div class="ft_footer">
+			<div class="ft_container">
+				<div class="ft_about">
+					<h4>Î ìàãàçèíå</h4>
+					<ul>
+						<li><a href="">Îòçûâû</a></li>
+						<li><a href="">Êîíòàêòû</a></li>
+						<li><a href="">Ðóêîâîäñòâî</a></li>
+						<li><a href="">Èñòîðèÿ</a></li>
+					</ul>
+				</div>
+				<div class="ft_catalog">
+					<h4>Êàòàëîã òîâàðîâ</h4>
+					<ul>
+						<li><a href="">Êóõíè</a></li>
+						<li><a href="">Êðîâàòè è êóøåòêè</a></li>
+						<li><a href="">Ãàðíèòóðû</a></li>
+						<li><a href="">Òóìîá÷êè è ïðèõîæèå</a></li>
+						<li><a href="">Ñïàëüíè è ìàòðàñû</a></li>
+						<li><a href="">Àêñåññóàðû</a></li>
+						<li><a href="">Ñòîëû è ñòóëüÿ</a></li>
+						<li><a href="">Êàòàëîãè ìåáåëè</a></li>
+						<li><a href="">Ðàñêëàäíûå äèâàíû</a></li>
+						<li><a href="">Êðåñëà</a></li>
+					</ul>
+					
+				</div>
+				<div class="ft_contacts">
+					<h4>Êîíòàêòíàÿ èíôîðìàöèÿ</h4>
+					<!-- vCard        http://help.yandex.ru/webmaster/hcard.pdf      -->
+					<p class="vcard">
+						<span class="adr">
+							<span class="street-address">óë. Ëåòíÿÿ ñòð.12, îôèñ 512</span>
+						</span>
+						<span class="tel">8 (495) 212-85-06</span>
+						<strong>Âðåìÿ ðàáîòû:</strong> <br/> <span class="workhours">åæåäíåâíî ñ 9-00 äî 18-00</span><br/>
+					</p>
+					<ul class="ft_solcial">
+						<li><a href="" class="fb"></a></li>
+						<li><a href="" class="tw"></a></li>
+						<li><a href="" class="ok"></a></li>
+						<li><a href="" class="vk"></a></li>
+					</ul>
+					<div class="ft_copyright">© 2000 - 2012 "Ìåáåëüíûé ìàãàçèí" </div>
+					
+					
+				</div>
+				
+				<div class="clearboth"></div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
---
+++ inner/header.php
@@ -0,0 +1,113 @@
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+IncludeTemplateLangFile(__FILE__);
+?>
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="windows-1251">
+	<title></title>
+	<link rel="stylesheet" href="template_style.css"/>
+	<script type="text/javascript" src="js/jquery-1.8.2.min.js"></script>
+	<script type="text/javascript" src="js/slides.min.jquery.js"></script>
+	<script type="text/javascript" src="js/jquery.carouFredSel-6.1.0-packed.js"></script>
+	<script type="text/javascript" src="js/functions.js"></script>
+	
+	<!--[if gte IE 9]><style type="text/css">.gradient {filter: none;}</style><![endif]-->
+</head>
+<body>
+	<div class="wrap">
+		<div class="hd_header_area">
+			<div class="hd_header">
+				<table>
+					<tr>
+						<td rowspan="2" class="hd_companyname">
+							<h1><a href="">Ìåáåëüíûé ìàãàçèí</a></h1>
+						</td>
+						<td rowspan="2" class="hd_txarea">
+							<span class="tel">8 (495) 212-85-06</span>	<br/>	
+							âðåìÿ ðàáîòû <span class="workhours">åæåäíåâíî ñ 9-00 äî 18-00</span>						
+						</td>
+						<td style="width:232px">
+							<form action="">
+								<div class="hd_search_form" style="float:right;">
+									<input placeholder="Ïîèñê" type="text"/>
+									<input type="submit" value=""/>
+								</div>
+							</form>
+						</td>
+					</tr>
+					<tr>
+						<td style="padding-top: 11px;">
+							<span class="hd_singin"><a id="hd_singin_but_open" href="">Âîéòè íà ñàéò</a>
+							<div class="hd_loginform">
+								<span class="hd_title_loginform">Âîéòè íà ñàéò</span>
+								<form name="" method="" action="">
+					
+									<input placeholder="Ëîãèí"  type="text">
+									<input  placeholder="Ïàðîëü"  type="password">			
+									<a href="/" class="hd_forgotpassword">Çàáûëè ïàðîëü</a>
+									
+									<div class="head_remember_me" style="margin-top: 10px">
+										<input id="USER_REMEMBER_frm" name="USER_REMEMBER" value="Y" type="checkbox">
+										<label for="USER_REMEMBER_frm" title="Çàïîìíèòü ìåíÿ íà ýòîì êîìïüþòåðå">Çàïîìíèòü ìåíÿ</label>
+									</div>				
+									<input value="Âîéòè" name="Login" style="margin-top: 20px;" type="submit">
+									</form>
+								<span class="hd_close_loginform">Çàêðûòü</span>
+							</div>
+							</span><br>
+							<a href="" class="hd_signup">Çàðåãèñòðèðîâàòüñÿ</a>
+						</td>
+					</tr>
+				</table>
+				<div class="nv_topnav">
+					<ul>
+						<li><a href=""   class="menu-img-fon"  style="background-image: url(images/nv_home.png);" ><span></span></a></li>
+						<li><a href=""><span>Êîìïàíèÿ</span></a>
+							<ul>
+								<li><a href="">Ïóíêò 1</a></li>
+								<li><a href="">Ïóíêò 2</a></li>
+								<li><a href="">Ïóíêò 3</a></li>
+								<li><a href="">Ïóíêò 4</a></li>
+							</ul>
+						</li>
+						<li><a href=""><span>Íîâîñòè</span></a></li>
+						<li><a href=""><span>Êàòàëîã</span></a></li>
+						<li><a href=""><span>Àêöèè</span></a>
+							<ul>
+								<li><a href="">Ïóíêò 1</a>
+									<ul>
+										<li><a href="">Ïóíêò 1</a></li>
+										<li><a href="">Ïóíêò 2</a></li>
+									</ul>
+								</li>
+								<li><a href="">Ïóíêò 2</a></li>
+								<li><a href="">Ïóíêò 3</a></li>
+								<li><a href="">Ïóíêò 4</a></li>
+							</ul>
+						</li>
+						<li><a href=""><span>Ïàðòíåðàì</span></a></li>
+						<li><a href=""><span>Êîíòàêòû</span></a></li>
+						<div class="clearboth"></div>
+					</ul>
+				</div>
+			</div>
+		</div>
+		
+		<!--- // end header area --->
+		<div class="bc_breadcrumbs">
+			<ul>
+				<li><a href="">Êàòàëîã</a></li>
+				<li><a href="">Ìåáåëü</a></li>
+				<li><a href="">Âûñòàâêè è ñîáûòèÿ</a></li>
+			</ul>
+			<div class="clearboth"></div>
+		</div>
+		<div class="main_container page">
+			<div class="mn_container">
+				<div class="mn_content">
+					<div class="main_post">
+						<div class="main_title">
+							<p class="title">Çàãîëîâîê ñòðàíèöû</p>
+						</div>
+						<!-- workarea -->
